f8f9e186a1b4bf87dd28cb60e998b59eeae3caa1
Connor Kormos <connorkormos97@gmail.com>
2023-08-22 15:49:10 -0700

    redid skills section

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -7,45 +7,42 @@ const Skills = forwardRef(({ skillsRef }) => {
     return (
         <div ref={skillsRef} className={styles.technicalSkillsWrapper}>
             <h1 className={styles.technicalSkillsH1}>Skills</h1>
+            <div className={styles.favoriteSkillsDiv}>
+                <h2 className={styles.divTitle}>Favorite Tech</h2>
+                <div className={styles.favoriteSkillsIconsDiv}>
+                    <img 
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" 
+                        className={styles.favoriteTechImage}
+                        alt="err"
+                        // style={{marginRight: '15%'}}
+                    />
+                    <img 
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" 
+                        className={styles.favoriteTechImage}
+                        alt="err"
+                        style={{marginTop: '1rem'}}
+                    />
+                    <img 
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" 
+                        className={styles.favoriteTechImage}
+                        alt="err"
+                    />
+                    <img 
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
+                        className={styles.favoriteTechImage}
+                        alt="err"
+                        style={{marginTop: '1rem'}}
+                    />
+                    <img 
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" 
+                        className={styles.favoriteTechImage}    
+                        alt="err"
+                    />
+                </div>
+            </div>
             <div className={styles.technicalSkillsDiv}>
-                <div className={styles.languagesDiv}>
-                    <h2>Languages</h2>
-                </div>
-                <div className={styles.favoriteSkillsDiv}>
-                    <h2>Favorite Tech</h2>
-                    <div className={styles.favoriteSkillsIconsDiv}>
-                        <img 
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg" 
-                            className={styles.favoriteTechImage}
-                            alt="err"
-                            // style={{marginRight: '15%'}}
-                        />
-                        <img 
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original-wordmark.svg" 
-                            className={styles.favoriteTechImage}
-                            alt="err"
-                        />
-                        <br></br>
-                        <img 
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original-wordmark.svg" 
-                            className={styles.favoriteTechImage}
-                            alt="err"
-                        />
-                        <br></br>
-                        <img 
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-plain-wordmark.svg"
-                            className={styles.favoriteTechImage}
-                            alt="err"
-                        />
-                        <img 
-                            src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original-wordmark.svg" 
-                            className={styles.favoriteTechImage}    
-                            alt="err"
-                        />
-                    </div>
-                </div>
                 <div className={styles.moreSkillsDiv}>
-                    <h2 style={{marginBottom: '-3%'}}>Additional Libraries <br></br>& Frameworks</h2>
+                    <h2 className={styles.divTitle}>Libraries, Frameworks, and More</h2>
                     <img 
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nextjs/nextjs-original-wordmark.svg" 
                         className={styles.techImage}    
@@ -66,7 +63,8 @@ const Skills = forwardRef(({ skillsRef }) => {
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/flask/flask-original-wordmark.svg" 
                         className={styles.techImage}
                         alt="err"
-                        />
+                    />
+                    <br></br>
                     <img 
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original-wordmark.svg" 
                         className={styles.techImage}
@@ -85,15 +83,13 @@ const Skills = forwardRef(({ skillsRef }) => {
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/eslint/eslint-original-wordmark.svg"
                         className={styles.techImage}
                     /> */}
-                    <img 
-                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" 
-                        className={styles.techImage}
-                        alt="err"
-                        />
                     {/* <img 
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/github/github-original-wordmark.svg" 
                         className={styles.techImage}
                     /> */}
+                </div>
+                <div className={styles.toolsDiv}>
+                    <h2 className={styles.divTitle}>More Tools</h2>
                     <img 
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jest/jest-plain.svg" 
                         className={styles.techImage}
@@ -108,9 +104,15 @@ const Skills = forwardRef(({ skillsRef }) => {
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/ubuntu/ubuntu-plain-wordmark.svg" 
                         className={styles.techImage}
                         alt="err"
-                        />
+                    />
+                    <br></br>
                     <img 
                         src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/visualstudio/visualstudio-plain-wordmark.svg" 
+                        className={styles.techImage}
+                        alt="err"
+                        />
+                    <img 
+                        src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original-wordmark.svg" 
                         className={styles.techImage}
                         alt="err"
                         />
