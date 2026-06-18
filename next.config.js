2a944c9f2834eac907eeb13ae49a9f12adb540e1
Connor Kormos <connorkormos97@gmail.com>
2026-06-17 19:58:07 -0700

    added imposter game to site

--- next.config.js
+++ next.config.js
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async rewrites() {
+    return {
+      beforeFiles: [
+        // Route /imposter to the impostergame build
+        {
+          source: "/imposter",
+          destination: "/impostergame/index.html",
+        },
+        {
+          source: "/imposter/:path*",
+          destination: "/impostergame/:path*",
+        },
+      ],
+    };
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
